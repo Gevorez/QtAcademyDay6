0222aff256ce776e8de753309c1dfafac0c05b57
Kamil Niewiarowski <niewiarowski.kamil.98@gmail.com>
2024-01-03 14:50:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,8 +8,7 @@ int main(int argc, char *argv[]) {
     qmlRegisterType<FileSystemModel>("FileSystemModel", 1, 0, "FileSystemModel");
 
     QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("/Users/kmr/QtAcademyCourse/Day6AppAlpha/Main.qml"));
-    engine.load(url);
+    engine.load((QUrl("qrc:/Day6AppAlpha/Main.qml")));
 
     return app.exec();
 }
